e447d5cd1850bb7660c48f887eea8b5e86b27612
Manjunath K <91064711+ManjunathO7@users.noreply.github.com>
2024-09-26 10:35:33 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -8,8 +8,8 @@ namespace TeleCommunicationColorCode
     enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
     enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
 
-     extern const char* MajorColorNames[5];
-     extern const char* MinorColorNames[5];
+     extern const char* MajorColorNames[];
+     extern const char* MinorColorNames[];
 
     class ColorPair {
     private:
